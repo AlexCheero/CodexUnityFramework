2dfa8aa84751e20d8ef062fca2a1b75490d7b3fc
Alexander Cherkasov <alexcheero157@gmail.com>
2025-05-04 12:54:19 +0200

    resetttable pooled children

---
+++ Utils/Pools/ResettablePooledChildren.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CodexFramework.Utils.Pools
+{
+    public class ResettablePooledChildren : PooledBehaviour, IResetOnGetPoolableBehaviour
+    {
+        [Serializable]
+#if UNITY_EDITOR
+        public
+#else
+        private
+#endif
+        struct ChildTransform
+        {
+            public Transform Transform;
+            public Vector3 LocalPosition;
+            public Quaternion LocalRotation;
+        }
+
+        [SerializeField]
+#if UNITY_EDITOR
+        public
+#else
+        private
+# endif
+        List<ChildTransform> _children;
+
+        public void OnGet()
+        {
+            foreach (var childTransform in _children)
+                childTransform.Transform.SetLocalPositionAndRotation(childTransform.LocalPosition, childTransform.LocalRotation);
+        }
+    }
+}
