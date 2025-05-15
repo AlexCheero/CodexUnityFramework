df5848f34339736379dc21cef78214e04f628541
Alexander Cherkasov <alexcheero157@gmail.com>
2025-05-15 18:15:04 +0200

    ParticleSystem.GetTotalDuration extension method

--- Utils/Utils.cs
+++ Utils/Utils.cs
@@ -18,6 +18,40 @@ using System.Reflection;
 
 namespace CodexFramework.Utils
 {
+    static class ParticleSystemExtensions
+    {
+        public static float GetTotalDuration(this ParticleSystem particleSystem)
+        {
+            float GetDuration(ParticleSystem ps)
+            {
+                var main = ps.main;
+                var delay = 0f;
+                if (main.startDelay.mode == ParticleSystemCurveMode.TwoConstants)
+                    delay = main.startDelay.constantMax;
+                else if (main.startDelay.mode == ParticleSystemCurveMode.Constant)
+                    delay = main.startDelay.constant;
+
+                var lifetime = 0f;
+                if (main.startLifetime.mode == ParticleSystemCurveMode.TwoConstants)
+                    lifetime = main.startLifetime.constantMax;
+                else if (main.startLifetime.mode == ParticleSystemCurveMode.Constant)
+                    lifetime = main.startLifetime.constant;
+
+                return delay + main.duration + lifetime;
+            }
+
+            var maxDuration = 0.0f;
+            foreach (var ps in particleSystem.GetComponentsInChildren<ParticleSystem>())
+            {
+                var duration = GetDuration(ps);
+                if (maxDuration < duration)
+                    maxDuration = duration;
+            }
+
+            return maxDuration;
+        }
+    }
+    
     [Serializable]
     public class SerializableDict<K, V>
     {
