0c6758c1f26eee247f5186308d79d9a7df171f33
Alexander Cherkasov <alexcheero157@gmail.com>
2025-05-15 14:48:58 +0200

    pooled rigidbody

---
+++ Utils/Pools/PooledRigidBody.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace CodexFramework.Utils.Pools
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class PooledRigidBody : PooledBehaviour, IResetOnGetPoolableBehaviour, IResetOnReturnPoolableBehaviour
+    {
+        private Rigidbody _rigidbody;
+        
+        void Awake() => _rigidbody = GetComponent<Rigidbody>();
+        
+        public void OnGet()
+        {
+            _rigidbody.velocity = _rigidbody.angularVelocity = Vector3.zero;
+            _rigidbody.ResetInertiaTensor();
+            _rigidbody.isKinematic = false;
+            _rigidbody.WakeUp();
+        }
+
+        public void OnReturn()
+        {
+            _rigidbody.isKinematic = true;
+            _rigidbody.Sleep();
+        }
+    }
+}
