d61601bbf3a775b96901690a41cdebbc62128cbe
Alexander Cherkasov <alexcheero157@gmail.com>
2025-05-25 22:22:37 +0200

    AudioSourceExtensions

---
+++ Utils/AudioSourceExtensions.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace CodexFramework.Utils
+{
+    public static class AudioSourceExtensions
+    {
+        public static void PlayOneShotRandomPitch(this AudioSource audioSource, AudioClip clip, float minPitch = 0.9f, float maxPitch = 1.1f)
+        {
+            audioSource.pitch = UnityEngine.Random.Range(minPitch, maxPitch);
+            audioSource.PlayOneShot(clip);
+        }
+    }
+}
