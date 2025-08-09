bb7c8296da1c420f3cf65511f9810e3066f19b8b
Alexander Cherkasov <alexcheero157@gmail.com>
2025-08-09 03:17:49 +0200

    always destroying Singleton._instance in OnDestroy

--- Utils/Singleton.cs
+++ Utils/Singleton.cs
@@ -55,7 +55,7 @@ namespace CodexFramework.Utils
 
         void OnDestroy()
         {
-            if (_dontDestroyOnLoad || _instance == null)
+            if (_instance == null)
                 return;
 
             Destroy(_instance.gameObject);
