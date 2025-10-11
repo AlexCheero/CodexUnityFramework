8629a2f9f87c9a7b660113afda584ecfe40f6a42
Alexander Cherkasov <alexcheero157@gmail.com>
2025-10-11 15:56:14 +0200

    windowing system

---
+++ WindowingSystem/WindowBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+[RequireComponent(typeof(Canvas))]
+public abstract class WindowBehaviour<T> : MonoBehaviour where T : Enum
+{
+    public event Action<T> OnOpen;
+    public event Action<WindowBehaviour<T>> OnClose;
+    
+    public abstract T GetWindowType();
+    
+    public Canvas Canvas { get; private set; }
+
+    public virtual void Init() => Canvas = GetComponent<Canvas>();
+    public virtual void Show() => gameObject.SetActive(true);
+    public virtual void Hide() => gameObject.SetActive(false);
+    protected void OnOpenClick(T window) => OnOpen?.Invoke(window);
+    public void OnCloseClick() => OnClose?.Invoke(this);
+}
---
+++ WindowingSystem/WindowsManager.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class WindowsManager<T> : MonoBehaviour where T : Enum
+{
+    private Dictionary<T, WindowBehaviour<T>> _windows;
+    
+    private Stack<WindowBehaviour<T>> _windowsStack;
+
+    private void Awake()
+    {
+        _windowsStack = new();
+        
+        var windows = GetComponentsInChildren<WindowBehaviour<T>>(true);
+        _windows = new();
+        foreach (var window in windows)
+        {
+            window.Init();
+#if DEBUG
+            if (_windows.ContainsKey(window.GetWindowType()))
+                throw new Exception($"Trying to add {window.name} by key {window.GetWindowType()} but {_windows[window.GetWindowType()].name} is already registered");
+#endif
+            _windows[window.GetWindowType()] = window;
+            window.OnOpen += ShowWindow;
+            window.OnClose += HideWindow;
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && HideLastWindow()) { }
+    }
+
+    private int _sortingOrder;
+    
+    public void ShowWindow(T windowType)
+    {
+        var window = _windows[windowType];
+        window.transform.SetAsLastSibling();
+        window.Canvas.sortingOrder = _sortingOrder;
+        window.Show();
+        _sortingOrder++;
+        
+        _windowsStack.Push(window);
+        if (_windowsStack.Count == 1)
+            OnFirstWindowOpened();
+    }
+
+    public bool HideLastWindow()
+    {
+        if (!_windowsStack.TryPop(out var window))
+            return false;
+        
+        window.transform.SetAsFirstSibling();
+        window.Canvas.sortingOrder = -1;
+        window.Hide();
+        _sortingOrder--;
+        
+        if (_windowsStack.Count == 0)
+            OnLastWindowClosed();
+        
+#if DEBUG
+        if (_sortingOrder < 0)
+            throw new Exception("Sorting order dropped below 0");
+#endif
+        
+        return true;
+    }
+
+    public void HideWindow(WindowBehaviour<T> window)
+    {
+        if (!_windowsStack.TryPeek(out var lastWindow) || lastWindow != window)
+        {
+#if DEBUG
+            throw new Exception("Trying to hide not last window");
+#else
+            return;
+#endif
+        }
+
+        HideLastWindow();
+    }
+
+    protected abstract void OnFirstWindowOpened();
+
+    protected abstract void OnLastWindowClosed();
+}
