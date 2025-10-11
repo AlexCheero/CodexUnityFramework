e5d6fc93886d6237c24eae2af3352ce428588c12
Alexander Cherkasov <alexcheero157@gmail.com>
2025-10-11 17:03:36 +0200

    WindowingSystem namespace

--- WindowingSystem/WindowBehaviour.cs
+++ WindowingSystem/WindowBehaviour.cs
@@ -1,19 +1,22 @@
 ﻿using System;
 using UnityEngine;
 
-[RequireComponent(typeof(Canvas))]
-public abstract class WindowBehaviour<T> : MonoBehaviour where T : Enum
+namespace CodexFramework.WindowingSystem
 {
-    public event Action<T> OnOpen;
-    public event Action<WindowBehaviour<T>> OnClose;
+    [RequireComponent(typeof(Canvas))]
+    public abstract class WindowBehaviour<T> : MonoBehaviour where T : Enum
+    {
+        public event Action<T> OnOpen;
+        public event Action<WindowBehaviour<T>> OnClose;
     
-    public abstract T GetWindowType();
+        public abstract T GetWindowType();
     
-    public Canvas Canvas { get; private set; }
+        public Canvas Canvas { get; private set; }
 
-    public virtual void Init() => Canvas = GetComponent<Canvas>();
-    public virtual void Show() => gameObject.SetActive(true);
-    public virtual void Hide() => gameObject.SetActive(false);
-    protected void OnOpenClick(T window) => OnOpen?.Invoke(window);
-    public void OnCloseClick() => OnClose?.Invoke(this);
+        public virtual void Init() => Canvas = GetComponent<Canvas>();
+        public virtual void Show() => gameObject.SetActive(true);
+        public virtual void Hide() => gameObject.SetActive(false);
+        protected void OnOpenClick(T window) => OnOpen?.Invoke(window);
+        public void OnCloseClick() => OnClose?.Invoke(this);
+    }
 }
--- WindowingSystem/WindowsManager.cs
+++ WindowingSystem/WindowsManager.cs
@@ -2,94 +2,97 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class WindowsManager<T> : MonoBehaviour where T : Enum
+namespace CodexFramework.WindowingSystem
 {
-    private Dictionary<T, WindowBehaviour<T>> _windows;
-    
-    private Stack<WindowBehaviour<T>> _windowsStack;
-
-    public bool IsAnyOpened => _windowsStack.Count > 0;
-
-    private void Awake()
+    public abstract class WindowsManager<T> : MonoBehaviour where T : Enum
     {
-        _windowsStack = new();
-        
-        var windows = GetComponentsInChildren<WindowBehaviour<T>>(true);
-        _windows = new();
-        foreach (var window in windows)
+        private Dictionary<T, WindowBehaviour<T>> _windows;
+    
+        private Stack<WindowBehaviour<T>> _windowsStack;
+
+        public bool IsAnyOpened => _windowsStack.Count > 0;
+
+        private void Awake()
         {
-            window.Init();
+            _windowsStack = new();
+        
+            var windows = GetComponentsInChildren<WindowBehaviour<T>>(true);
+            _windows = new();
+            foreach (var window in windows)
+            {
+                window.Init();
 #if DEBUG
-            if (_windows.ContainsKey(window.GetWindowType()))
-                throw new Exception($"Trying to add {window.name} by key {window.GetWindowType()} but {_windows[window.GetWindowType()].name} is already registered");
+                if (_windows.ContainsKey(window.GetWindowType()))
+                    throw new Exception($"Trying to add {window.name} by key {window.GetWindowType()} but {_windows[window.GetWindowType()].name} is already registered");
 #endif
-            _windows[window.GetWindowType()] = window;
-            window.OnOpen += ShowWindow;
-            window.OnClose += HideWindow;
+                _windows[window.GetWindowType()] = window;
+                window.OnOpen += ShowWindow;
+                window.OnClose += HideWindow;
+            }
         }
-    }
 
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Escape) && HideLastWindow()) { }
-    }
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape) && HideLastWindow()) { }
+        }
 
-    private int _sortingOrder;
+        private int _sortingOrder;
     
-    public void ShowWindow(T windowType)
-    {
-        if (_windowsStack.TryPeek(out var lastWindow))
-            lastWindow.Hide();
+        public void ShowWindow(T windowType)
+        {
+            if (_windowsStack.TryPeek(out var lastWindow))
+                lastWindow.Hide();
         
-        var window = _windows[windowType];
-        window.transform.SetAsLastSibling();
-        window.Canvas.sortingOrder = _sortingOrder;
-        window.Show();
-        _sortingOrder++;
+            var window = _windows[windowType];
+            window.transform.SetAsLastSibling();
+            window.Canvas.sortingOrder = _sortingOrder;
+            window.Show();
+            _sortingOrder++;
         
-        _windowsStack.Push(window);
-        if (_windowsStack.Count == 1)
-            OnFirstWindowOpened();
-    }
+            _windowsStack.Push(window);
+            if (_windowsStack.Count == 1)
+                OnFirstWindowOpened();
+        }
 
-    public bool HideLastWindow()
-    {
-        if (!_windowsStack.TryPop(out var window))
-            return false;
+        public bool HideLastWindow()
+        {
+            if (!_windowsStack.TryPop(out var window))
+                return false;
         
-        window.transform.SetAsFirstSibling();
-        window.Canvas.sortingOrder = -1;
-        window.Hide();
-        _sortingOrder--;
+            window.transform.SetAsFirstSibling();
+            window.Canvas.sortingOrder = -1;
+            window.Hide();
+            _sortingOrder--;
         
-        if (_windowsStack.Count == 0)
-            OnLastWindowClosed();
-        else
-            _windowsStack.Peek().Show();
+            if (_windowsStack.Count == 0)
+                OnLastWindowClosed();
+            else
+                _windowsStack.Peek().Show();
         
 #if DEBUG
-        if (_sortingOrder < 0)
-            throw new Exception("Sorting order dropped below 0");
+            if (_sortingOrder < 0)
+                throw new Exception("Sorting order dropped below 0");
 #endif
         
-        return true;
-    }
+            return true;
+        }
 
-    public void HideWindow(WindowBehaviour<T> window)
-    {
-        if (!_windowsStack.TryPeek(out var lastWindow) || lastWindow != window)
+        public void HideWindow(WindowBehaviour<T> window)
         {
+            if (!_windowsStack.TryPeek(out var lastWindow) || lastWindow != window)
+            {
 #if DEBUG
-            throw new Exception("Trying to hide not last window");
+                throw new Exception("Trying to hide not last window");
 #else
             return;
 #endif
+            }
+
+            HideLastWindow();
         }
 
-        HideLastWindow();
+        protected abstract void OnFirstWindowOpened();
+
+        protected abstract void OnLastWindowClosed();
     }
-
-    protected abstract void OnFirstWindowOpened();
-
-    protected abstract void OnLastWindowClosed();
 }
