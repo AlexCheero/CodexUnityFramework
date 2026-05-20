8e76e705a07ba6c89a386415252b9ef01a8ea98c
Alexander Cherkasov <alexcheero157@gmail.com>
2026-05-20 18:38:23 +0200

    WindowType getter and _window field moved into base WindowBehaviour

--- WindowingSystem/WindowBehaviour.cs
+++ WindowingSystem/WindowBehaviour.cs
@@ -5,7 +5,10 @@ namespace CodexFramework.WindowingSystem
 {
     public abstract class WindowBehaviour<T> : MonoBehaviour where T : Enum
     {
-        public abstract T WindowType { get; }
+        [SerializeField]
+        private T _window;
+    
+        public T WindowType => _window;
         
         public virtual void Show() => gameObject.SetActive(true);
         public virtual void Hide() => gameObject.SetActive(false);
