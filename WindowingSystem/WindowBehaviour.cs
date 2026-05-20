2abe9615574f4229faca1ccb97d6559951c82fe4
Alexander Cherkasov <alexcheero157@gmail.com>
2026-05-20 19:04:45 +0200

    protected window type

--- WindowingSystem/WindowBehaviour.cs
+++ WindowingSystem/WindowBehaviour.cs
@@ -6,9 +6,9 @@ namespace CodexFramework.WindowingSystem
     public abstract class WindowBehaviour<T> : MonoBehaviour where T : Enum
     {
         [SerializeField]
-        private T _window;
+        protected T _window;
     
-        public T WindowType => _window;
+        public virtual T WindowType => _window;
         
         public virtual void Show() => gameObject.SetActive(true);
         public virtual void Hide() => gameObject.SetActive(false);
