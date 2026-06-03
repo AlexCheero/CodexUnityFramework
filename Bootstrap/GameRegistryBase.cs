84989759dd5418e6beab1ca07e1b4fe81ae281b7
Alexander Cherkasov <alexcheero157@gmail.com>
2026-06-03 20:28:53 +0200

    GameRegistryBase.IsCreated removed

--- Bootstrap/GameRegistryBase.cs
+++ Bootstrap/GameRegistryBase.cs
@@ -5,8 +5,6 @@ namespace CodexFramework.Bootstrap
         private static TSelf _instance;
         public static TSelf Instance => _instance ??= new TSelf();
 
-        public static bool IsCreated => _instance != null;
-
         public static void Initialize() => Instance.OnInitialize();
 
         protected abstract void OnInitialize();
